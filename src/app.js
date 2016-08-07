3c32262a6c24bd89c47a6e5796a56c62752cd765
snicusor <smarandanicusor@yahoo.com>
2016-08-07 13:19:34 +0100

    added a router and syncHistoryWithStore from reac-router-redux

--- src/app.js
+++ src/app.js
@@ -2,26 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import * as reducers from './reducers';
+reducers.routing = routerReducer;
+
 import App from './components/App';
-import Sidebar from './components/Sidebar';
 
 const store = createStore(combineReducers(reducers));
+const history = syncHistoryWithStore(browserHistory, store);
 
-//Render function
 function run () {
   let state = store.getState();
-  console.log(state);
-  ReactDOM.render((<Provider store={store}>
-    <App>
-      <Sidebar />
-    </App>
+  //console.log(state);
+  ReactDOM.render((
+    <Provider store={store}>
+      <Router history={history}>
+        <Route path="/" component={App} />
+      </Router>
     </Provider>),
-    document.getElementById('root')
-  );
+    document.getElementById('root'));
 }
 
 run();
-
 store.subscribe(run);
 
