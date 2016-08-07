382450a132ee5a9a8d3a88baa154cbe58b1cc96d
snicusor <smarandanicusor@yahoo.com>
2016-08-07 20:42:22 +0100

    added  the edit card  modal component

--- src/actions.js
+++ src/actions.js
@@ -3,3 +3,5 @@ export const showAddDeck = () => ({ type: 'SHOW_ADD_DECK'});
 export const hideAddDeck = () => ({ type: 'HIDE_ADD_DECK'});
 
 export const addCard = card => ({ type: 'ADD_CARD', data: card });
+export const updateCard = card   => ({ type: 'UPDATE_CARD', data: card   });
+export const deleteCard = cardId => ({ type: 'DELETE_CARD', data: cardId });
--- src/reducers.js
+++ src/reducers.js
@@ -1,15 +1,27 @@
 export const cards = (state, action) => {
   switch (action.type) {
+    case 'RECEIVE_DATA':
+      return action.data.cards || state;
     case 'ADD_CARD':
-      let newCard = Object.assign({}, state, {
+      let newCard = Object.assign({}, action.data, {
         score: 1,
         id: +new Date
       });
+
       return state.concat([newCard]);
+    case 'UPDATE_CARD':
+      let cardUpdate = action.data;
+      return state.map(card => (card.id !== cardUpdate.id) ?
+        card :
+        Object.assign({}, card, cardUpdate)
+      );
+    case 'DELETE_CARD':
+      return state.filter(c => c.id !== action.data);
     default:
       return state || [];
   }
 };
+
 export const decks = (state, action) => {
   switch (action.type) {
     case 'ADD_DECK':
