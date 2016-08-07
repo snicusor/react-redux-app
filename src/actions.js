7004fbdbe436e865b103c449b06aa7274d82006d
snicusor <smarandanicusor@yahoo.com>
2016-08-07 20:50:24 +0100

    added new action and reducer and updated visibleCards component to be able to filter cards

--- src/actions.js
+++ src/actions.js
@@ -5,3 +5,6 @@ export const hideAddDeck = () => ({ type: 'HIDE_ADD_DECK'});
 export const addCard = card => ({ type: 'ADD_CARD', data: card });
 export const updateCard = card   => ({ type: 'UPDATE_CARD', data: card   });
 export const deleteCard = cardId => ({ type: 'DELETE_CARD', data: cardId });
+
+export const filterCards = query => ({ type: 'FILTER_CARDS', data: query });
+export const setShowBack = back  => ({ type: 'SHOW_BACK', data: back });
