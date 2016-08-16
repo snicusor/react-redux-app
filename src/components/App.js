b7d8518b551ad38ab91ea3a373b5f311675015ac
snicusor <smarandanicusor@yahoo.com>
2016-08-16 13:58:46 +0100

    added start script

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,16 @@ import Sidebar from './Sidebar';
 import Toolbar from './Toolbar';
 import { connect } from 'react-redux';
 
-const mapStateToProps = (props, {params: {deckId} }) => ({
+const mapStateToProps = (props, { params: { deckId } }) => ({
   deckId
 });
 
-const App = (deckId, props) => {
-  return (
-    <div className="app">
-      <Toolbar deckId={deckId} />
-      <Sidebar />
-      {props.children}
-    </div>
-  );
+const App = ({ deckId, children }) => {
+  return (<div className='app'>
+    <Toolbar deckId={deckId} />
+    <Sidebar />
+    {children}
+  </div>);
 };
 
 export default connect(mapStateToProps)(App);
